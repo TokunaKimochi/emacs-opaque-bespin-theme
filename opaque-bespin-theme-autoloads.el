b216d2ea169b6f5a852bb48056bc33983114d69b
TokunaKimochi <tokunakimochi@gmail.com>
2018-06-03 14:32:38 +0900

    自動生成ファイルを削除

--- opaque-bespin-theme-autoloads.el
+++
@@ -1,19 +0,0 @@
-;;; opaque-bespin-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "opaque-bespin-theme" "opaque-bespin-theme.el"
-;;;;;;  (23314 25337 0 0))
-;;; Generated autoloads from opaque-bespin-theme.el
-
-(when (and (boundp 'custom-theme-load-path) load-file-name) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; opaque-bespin-theme-autoloads.el ends here
