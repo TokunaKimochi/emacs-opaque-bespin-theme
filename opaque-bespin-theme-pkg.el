f019491c821415556d366db35b1ce2ece888f309
TokunaKimochi <tokunakimochi@gmail.com>
2018-06-03 10:09:29 +0900

    セカンド・コミット

---
+++ opaque-bespin-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "opaque-bespin-theme" "20180602.1126" "A Theme based on the colors of the material-theme.el" '((emacs "24.1")) :authors '(("Christoph Paulik" . "cpaulik@gmail.com")) :maintainer '("Christoph Paulik" . "cpaulik@gmail.com") :url "http://github.com/cpaulik/emacs-material-theme")
